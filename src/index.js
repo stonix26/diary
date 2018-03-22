22ca6565ce9a2b3ddb5e797525e04e24b37f0e29
Ruston Emperua <rustonemperua@elink.com.ph>
2018-03-22 17:18:14 +0800

    creating redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+// Redux
+import { createStore, applyMiddleWare } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+
+// Create redux store -> reducers -> 'actions' | applyMiddleWare()
+
+const store = createStore(composeWithDevTools());
+
+// Provide the store to react
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
