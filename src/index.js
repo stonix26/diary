b04d1b7d7ee2deff7014ba09e7bc7ec22356d5ea
Ruston Emperua <rustonemperua@elink.com.ph>
2018-03-22 21:02:27 +0800

    passing reducers to redux store

--- src/index.js
+++ src/index.js
@@ -6,14 +6,19 @@ import registerServiceWorker from './registerServiceWorker';
 // Redux
 import { createStore, applyMiddleWare } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
 
 
 // Create redux store -> reducers -> 'actions - actionType' | applyMiddleWare()
 
-const store = createStore(composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 // Provide the store to react
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
